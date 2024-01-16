a2017af2063a6b6188360c63a190aed12099c6c3
shubhamB0620 <shubhambarangule0706@gmail.com>
2024-01-16 17:44:42 +0530

    Convertion Opertion

---
+++ JS/script.js
@@ -0,0 +1,15 @@
+/*Data Type */
+let string = "123"
+let int = 121
+let boolean = true
+let nullval = null
+let un //undefined
+let symbol
+document.write(typeof nullval);
+
+/* Convertion Opertion */
+console.log(typeof string)
+let changeValue = Number(string)
+console.log(changeValue)
+let changeValue2 = Number(boolean)
+console.log(changeValue2)
